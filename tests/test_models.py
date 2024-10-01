5c855c2e567e180236d11a313bba3196ebd87274
ShekharNarayanan <shekharnarayanan833@gmail.com>
2024-10-01 12:19:23 +0200

    Formatting changes

--- tests/test_models.py
+++ tests/test_models.py
@@ -43,7 +43,6 @@ def test_daily_min(test, expected):
     npt.assert_array_equal(daily_min(test), expected)
 
 
-
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
